37a1dd68649ac5fd5422479a20716604d8735bc3
Magyar Máté András <magyarm@ukatemi.com>
2023-08-29 14:44:49 +0200

    Update to v0.0.2 (#8)

--- man/conf.py
+++ man/conf.py
@@ -9,7 +9,7 @@
 project = 'SIMBIOTA Client'
 copyright = '2023, Ukatemi Technologies .'
 author = 'Ukatemi Technologies Zrt.'
-release = '0.0.1'
+release = '0.0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
