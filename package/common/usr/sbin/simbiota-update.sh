3f59ef1fd08b1e150e64b27fa261bc3c5e1ce0d2
tcsh4ck <83782641+tcsh4ck@users.noreply.github.com>
2023-09-12 15:29:58 +0200

    Improved logging and device support (#10)
    * improved logging
    
    * added libc and libgcc package dependencies
    
    * changed build to buster in order to support legacy raspbian as well, we only used one function from libc 2.29
    
    * updated simbiota-update.sh with description and workaround for setpriv bug on legacy raspbian
    
    * removed unnecessary code
    
    ---------
    
    Co-authored-by: Csongor Tamás <csongor.tamas@ukatemi.com>

--- package/common/usr/sbin/simbiota-update.sh
+++ package/common/usr/sbin/simbiota-update.sh
@@ -3,7 +3,11 @@
 set -o errexit
 set -o nounset
 
-setpriv --reuid=nobody --regid=nogroup --init-groups --inh-caps=-all sh -s <<- EOF
+# Dropping privileges because downloading a file doesn't need root.
+# The --inh-caps part is essentially the same as -all but on legacy raspbian
+# `setpriv` returns 'setpriv: libcap-ng is too old for "all" caps' so
+# we list all priviliges one-by-one with --list-caps.
+setpriv --reuid=nobody --regid=nogroup --init-groups --inh-caps="$(setpriv --list-caps | xargs -I{} printf '-{},' | head -c-1)" sh -s <<- EOF
 umask 0000
 curl -s https://api.github.com/repos/simbiota/database-releases/releases/latest \
 | jq '.assets[] | select(.name | match(".*-arm-.*")).browser_download_url' \
--- simbiota/src/detection_system.rs
+++ simbiota/src/detection_system.rs
@@ -73,17 +73,6 @@ pub enum CommandResult {
     QuarantineAction(bool),
 }
 
-static mut VERBOSE_LOG: bool = false;
-macro_rules! bg_log {
-    ($level:expr, $($arg:tt)+) => {
-        let verbose = unsafe { VERBOSE_LOG };
-        if verbose {
-            // safe as the logger does no file accesses
-            (log!($level, $($arg)+))
-        }
-    };
-}
-
 impl DetectionSystem {
     pub fn register_provider(name: &str, provider: Arc<dyn DetectorProvider + Send + Sync>) {
         REGISTERED_PROVIDERS
@@ -104,9 +93,6 @@ impl DetectionSystem {
         database: Arc<Mutex<SystemDatabase>>,
         verbose_log: bool,
     ) -> Self {
-        unsafe {
-            VERBOSE_LOG = verbose_log;
-        }
         let detector_config = &client_config.detector;
         let class = &detector_config.class;
 
@@ -330,7 +316,7 @@ impl DetectionSystem {
     fn detector_callback(&self, event_meta: &fanotify_event_metadata) -> FanotifyEventResponse {
         if event_meta.pid as u32 == self.daemon_pid {
             // ignore accesses from myself
-            bg_log!(log::Level::Debug, "ignoring access from myself");
+            debug!("ignoring access from myself");
             return FanotifyEventResponse::Allow;
         }
 
@@ -343,49 +329,42 @@ impl DetectionSystem {
         let filename = maybe_filename.unwrap_or_else(|| "<n/a>".to_string());
         let orig_fname = filename.clone();
 
+        info!("checking file: {}", filename);
         // check cache first
         if has_filename {
             if let Some(result) = self.cache.borrow().get_result_for(&filename, event_meta) {
                 let detection_duration = detect_start_ts.elapsed();
 
-                bg_log!(
-                    log::Level::Debug,
-                    "cached scan took: {:?}",
+                debug!(
+                    "scanning took: {:?} (cached)",
                     detection_duration.clone()
                 );
                 return if result == DetectionResult::NoMatch {
-                    let logname = filename.clone();
-                    bg_log!(
-                        log::Level::Info,
-                        "cached detection negative for {}",
-                        logname
+                    info!(
+                        "detection negative: {} (cached)",
+                        filename
                     );
                     Allow
                 } else {
-                    let logname = filename.clone();
-                    bg_log!(log::Level::Error, "cached detection positive: {}", logname);
+                    error!("detection positive: {} (cached)", filename);
                     self.file_detected_action(filename.clone());
                     Deny
                 };
             }
         }
-        let logname = filename.clone();
-        bg_log!(log::Level::Info, "checking file: {}", logname);
         let mut no_cache = false;
         let mut res = self
             .detector
             .borrow_mut()
             .check_reader(&mut file)
             .unwrap_or_else(|e| {
-                let logname = filename.clone();
-                bg_log!(log::Level::Error, "error checking file: {}", logname);
+                warn!("error checking file: {} ({})", filename, e);
                 no_cache = true; // skip caching this result
                 DetectionResult::NoMatch
             });
 
         let detection_duration = detect_start_ts.elapsed();
-        bg_log!(
-            log::Level::Debug,
+        debug!(
             "scanning took: {:?}",
             detection_duration.clone()
         );
@@ -397,15 +376,14 @@ impl DetectionSystem {
         }
 
         if res == DetectionResult::Match {
-            bg_log!(log::Level::Info, "file matched: {}", filename);
+            error!("detection positive: {}", filename);
             self.file_detected_action(orig_fname);
-            bg_log!(log::Level::Debug, "detected actions done");
+            debug!("detected actions done");
         } else {
-            bg_log!(log::Level::Info, "detection negative for {}", filename);
+            info!("detection negative: {}", filename);
         }
 
-        bg_log!(
-            log::Level::Debug,
+        debug!(
             "blocking took: {:?}",
             detect_start_ts.elapsed()
         );
@@ -427,12 +405,11 @@ impl DetectionSystem {
             };
 
             if let Some(quarantine) = &quarantine {
-                bg_log!(log::Level::Error, "moving file to quarantine");
+                error!("moving file to quarantine: {}", filename);
                 quarantine.lock().unwrap().add_file(&filename);
             } else {
-                bg_log!(
-                    log::Level::Warn,
-                    "cannot move file to quarantine: quarantine disabled"
+                info!(
+                    "not moving file to quarantine: quarantine disabled"
                 );
             }
 
